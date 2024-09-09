60ce1378e5ef7244b15edab1faf1da7d78357690
Lengocanh <Lengocanh3524@gmail.com>
2024-09-10 00:20:34 +0700

    create demo cuculator

---
+++ src/main/java/demoDefaultPackage.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class demoDefaultPackage {
+    public static void main(String[] args) {
+        CircularButtonExample cl = new CircularButtonExample();
+        cl.show();
+    }
+}
